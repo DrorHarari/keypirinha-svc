9ce16fc78da5a278d8b41bc32c81f3a7a3a5c147
Dror Harari <dror.harari@gmail.com>
2022-04-19 02:15:15 +0300

    Get service state
    Still skeleton

--- svc.py
+++ svc.py
@@ -1,6 +1,6 @@
 import keypirinha as kp
 import keypirinha_util as kpu
-from .svcutil import WinServiceUtils
+from .svcutil import *
 import os
 
 class Svc(kp.Plugin):
@@ -17,20 +17,14 @@ class Svc(kp.Plugin):
     ITEMACT_PAUSE    = "pause"
     ITEMACT_RESUME   = "resume"
     
-    service_catalog = {}
+    _service_catalog = {}
+    _wsu = None
+
 
     def __init__(self):
         super().__init__()
-        
-        start_action = self.create_action(name=self.ITEMACT_START, label="Start", short_desc="Start service")
-        stop_action = self.create_action(name=self.ITEMACT_STOP, label="Stop", short_desc="Stop service")
-        restart_action = self.create_action(name=self.ITEMACT_RESTART, label="Restart", short_desc="Restart service")
-        pause_action = self.create_action(name=self.ITEMACT_PAUSE, label="Pause", short_desc="Pause service")
-        resume_action = self.create_action(name=self.ITEMACT_RESUME, label="Resume", short_desc="Resume service")
 
-        self.set_actions(self.ITEMCAT_RUNNING, [stop_action, restart_action, pause_action])
-        self.set_actions(self.ITEMCAT_STOPPED, [start_action, restart_action, resume_action])
-        self.set_actions(self.ITEMCAT_PAUSED, [stop_action, resume_action, restart_action])
+        self._wsu = WinServiceUtils()
 
     def on_start(self):
         pass
@@ -46,8 +40,7 @@ class Svc(kp.Plugin):
 #            self.on_catalog()
         
     def on_catalog(self):
-        wsu = WinServiceUtils()
-        self.service_catalog = wsu.EnumServicesStatus(0x30, 0x3)
+        self._service_catalog = self._wsu.EnumServicesStatus(0x30, 0x3)
 
         catalog = [
             self.create_item(
@@ -60,27 +53,62 @@ class Svc(kp.Plugin):
         ]
 
         self.set_catalog(catalog)
+       
+        start_action = self.create_action(name=self.ITEMACT_START, label="Start", short_desc="Start service")
+        stop_action = self.create_action(name=self.ITEMACT_STOP, label="Stop", short_desc="Stop service")
+        restart_action = self.create_action(name=self.ITEMACT_RESTART, label="Restart", short_desc="Restart service")
+        pause_action = self.create_action(name=self.ITEMACT_PAUSE, label="Pause", short_desc="Pause service")
+        resume_action = self.create_action(name=self.ITEMACT_RESUME, label="Resume", short_desc="Resume service")
+
+        self.set_actions(self.ITEMCAT_RUNNING, [stop_action, restart_action, pause_action])
+        self.set_actions(self.ITEMCAT_STOPPED, [start_action, restart_action, resume_action])
+        self.set_actions(self.ITEMCAT_PAUSED, [stop_action, resume_action, restart_action])
 
     def set_service_category(self, user_input, items_chain):
         item = items_chain[-1]
-        sn = item.target()
+        service_name = item.target()
         suggestions = []
-        suggestions.append(
-            self.create_item(
-                    category = self.ITEMCAT_ACTION,
-                    label = f"Start service {sn}",
-                    short_desc = "Start service",
-                    target = "start",
-                    args_hint = kp.ItemArgsHint.FORBIDDEN,
-                    hit_hint = kp.ItemHitHint.NOARGS))
-        suggestions.append(
-            self.create_item(
-                    category = self.ITEMCAT_ACTION,
-                    label = f"Stop service {sn}",
-                    short_desc = "Stop service",
-                    target = "stop",
-                    args_hint = kp.ItemArgsHint.FORBIDDEN,
-                    hit_hint = kp.ItemHitHint.NOARGS))
+
+        service_status = self._wsu.QueryServiceStatusEx(service_name)
+
+        if service_status.current_state in [self._wsu.SERVICE_RUNNING]:
+            suggestions.append(
+                self.create_item(
+                        category = self.ITEMCAT_RUNNING,
+                        label = f"Running service {service_name}",
+                        short_desc = "Running service",
+                        target = service_name,
+                        args_hint = kp.ItemArgsHint.FORBIDDEN,
+                        hit_hint = kp.ItemHitHint.NOARGS))
+        elif service_status.current_state in [self._wsu.SERVICE_STOPPED]:
+            suggestions.append(
+                self.create_item(
+                        category = self.ITEMCAT_STOPPED,
+                        label = f"Stopped service {service_name}",
+                        short_desc = "Stopped service",
+                        target = service_name,
+                        args_hint = kp.ItemArgsHint.FORBIDDEN,
+                        hit_hint = kp.ItemHitHint.NOARGS))
+
+
+# SERVICE_STOPPED         
+# SERVICE_START_PENDING           suggestions = []
+# SERVICE_STOP_PENDING            suggestions.append(
+# SERVICE_RUNNING                     self.create_item(
+# SERVICE_CONTINUE_PENDING                    category = self.ITEMCAT_ACTION,
+# SERVICE_PAUSE_PENDING                       label = f"Start service {service_name}",
+# SERVICE_PAUSED                              short_desc = "Start service",
+                    # target = "start",
+                    # args_hint = kp.ItemArgsHint.FORBIDDEN,
+                    # hit_hint = kp.ItemHitHint.NOARGS))
+        # suggestions.append(
+            # self.create_item(
+                    # category = self.ITEMCAT_ACTION,
+                    # label = f"Stop service {service_name}",
+                    # short_desc = "Stop service",
+                    # target = "stop",
+                    # args_hint = kp.ItemArgsHint.FORBIDDEN,
+                    # hit_hint = kp.ItemHitHint.NOARGS))
 
         self.set_suggestions(suggestions, kp.Match.ANY, kp.Sort.NONE)
 
@@ -91,10 +119,6 @@ class Svc(kp.Plugin):
             return
 
         current_item = items_chain[-1]
-
-        print(f"on_suggest {user_input} - #items = {len(items_chain)}")
-        if (items_chain):
-            print(f"categoey = {items_chain[-1].category()}, target={items_chain[-1].target()}")
 
         if items_chain and items_chain[-1].category() == self.ITEMCAT_SVC:
             self.set_service_category(user_input, items_chain)
@@ -108,14 +132,13 @@ class Svc(kp.Plugin):
             user_input = user_input.lower()
 
         suggestions = []
-        for sn in self.service_catalog:
-            s = self.service_catalog[sn]
-            if user_input in s["service_name"].lower():
+        for (service_name, service) in self._service_catalog.items():
+            if user_input.lower() in service_name:
                 suggestions.append(self.create_item(
                     category=self.ITEMCAT_SVC,
-                    label=f'Service {s["service_name"]}',
-                    short_desc=s["display_name"],
-                    target=sn,
+                    label=f'Service {service_name}',
+                    short_desc=bytes.decode(service.display_name, "mbcs"),
+                    target=service_name,
                     loop_on_suggest = True,
                     args_hint=kp.ItemArgsHint.FORBIDDEN,
                     hit_hint=kp.ItemHitHint.IGNORE))
--- svcutil.py
+++ svcutil.py
@@ -5,23 +5,6 @@
 import ctypes as ct
 import sys
 
-def declare_fnc(ct_dll, func_name, ret=None, args=[]):
-    fnc = getattr(ct_dll, func_name)
-    fnc.restype = ret
-    fnc.argtypes = args
-    return fnc
-
-_advapi32 = ct.windll.advapi32
-
-_OpenSCManager = declare_fnc(_advapi32, "OpenSCManagerA", ct.c_void_p,          \
-    [ct.c_char_p, ct.c_char_p, ct.c_ulong])
-
-_OpenService = declare_fnc(_advapi32, "OpenServiceA", ct.c_void_p,              \
-    [ct.c_void_p, ct.c_char_p, ct.c_ulong])
-
-_CloseServiceHandle = declare_fnc(_advapi32, "CloseServiceHandle", ct.c_bool,   \
-    [ct.c_void_p])
-
 class SERVICE_STATUS_PROCESS(ct.Structure):
     _fields_ = [
         ("service_type", ct.c_ulong),
@@ -35,56 +18,90 @@ class SERVICE_STATUS_PROCESS(ct.Structure):
         ("service_flags", ct.c_ulong)
     ]
 
-
-SC_STATUS_PROCESS_INFO = 0  # QueryServiceStatusEx level 
-_QueryServiceStatusEx = declare_fnc(_advapi32, "QueryServiceStatusEx",  ct.c_bool,  \
-    [ct.c_void_p, ct.c_ulong, ct.POINTER(SERVICE_STATUS_PROCESS), ct.c_void_p, ct.c_void_p])
-#    [ct.c_void_p, ct.c_ulong, ct.c_char_p, ct.c_void_p, ct.c_void_p])
-
-class ENUM_SERVICE_STATUS(ct.Structure):
-    _fields_ = [
-        ("service_name", ct.c_char_p), 
-        ("display_name", ct.c_char_p), 
-        ("service_status", ct.c_ulong), 
-        ("service_type", ct.c_ulong),
-        ("current_state", ct.c_ulong),
-        ("controls_accepted", ct.c_ulong),
-        ("win_exit_code", ct.c_ulong),
-        ("service_exit_code", ct.c_ulong),
-        ("check_point", ct.c_ulong),
-        ("wait_hint", ct.c_ulong)
-    ]
-
-_EnumServicesStatus = declare_fnc(_advapi32, "EnumServicesStatusA", ct.c_bool,  \
-    [ct.c_void_p, ct.c_ulong, ct.c_ulong, ct.c_char_p, ct.c_ulong, ct.c_void_p, ct.c_void_p, ct.c_void_p])
-
-
-# Service types
-ST_SERVICE_DRIVER 				= 0x0000000B
-ST_SERVICE_FILE_SYSTEM_DRIVER	= 0x00000002
-ST_SERVICE_KERNEL_DRIVER		= 0x00000001
-ST_SERVICE_WIN32				= 0x00000030
-ST_SERVICE_WIN32_OWN_PROCESS	= 0x00000010
-ST_SERVICE_WIN32_SHARE_PROCESS	= 0x00000020
-
-# Service states
-SS_SERVICE_ACTIVE		= 0x00000001
-SS_SERVICE_INACTIVE		= 0x00000002
-SS_SERVICE_STATE_ALL	= 0x00000003
-
-# Service control access options
-SC_MANAGER_CONNECT              = 0x0001
-SC_MANAGER_ENUMERATE_SERVICE    = 0x0004
-SC_MANAGER_ALL_ACCESS           = 0x000f003f
-
-# Windows error codes
-ERROR_INSUFFICIENT_BUFFER   = 122
-GENERIC_EXECUTE             = 0x20000000
-GENERIC_WRITE               = 0x40000000
-GENERIC_READ                = 0x80000000
-
+    class ENUM_SERVICE_STATUS(ct.Structure):
+        _fields_ = [
+            ("service_name", ct.c_char_p), 
+            ("display_name", ct.c_char_p), 
+            ("service_status", ct.c_ulong), 
+            ("service_type", ct.c_ulong),
+            ("current_state", ct.c_ulong),
+            ("controls_accepted", ct.c_ulong),
+            ("win_exit_code", ct.c_ulong),
+            ("service_exit_code", ct.c_ulong),
+            ("check_point", ct.c_ulong),
+            ("wait_hint", ct.c_ulong)
+        ]
+        
 class WinServiceUtils:
-  
+    # Service types
+    ST_SERVICE_DRIVER 				= 0x0000000B
+    ST_SERVICE_FILE_SYSTEM_DRIVER	= 0x00000002
+    ST_SERVICE_KERNEL_DRIVER		= 0x00000001
+    ST_SERVICE_WIN32				= 0x00000030
+    ST_SERVICE_WIN32_OWN_PROCESS	= 0x00000010
+    ST_SERVICE_WIN32_SHARE_PROCESS	= 0x00000020
+
+    # Service states for enumeration
+    SS_SERVICE_ACTIVE		= 0x00000001
+    SS_SERVICE_INACTIVE		= 0x00000002
+    SS_SERVICE_STATE_ALL	= 0x00000003
+
+    # Service States for CurrentState
+    SERVICE_STOPPED         =              0x00000001
+    SERVICE_START_PENDING   =              0x00000002
+    SERVICE_STOP_PENDING    =              0x00000003
+    SERVICE_RUNNING         =              0x00000004
+    SERVICE_CONTINUE_PENDING=              0x00000005
+    SERVICE_PAUSE_PENDING   =              0x00000006
+    SERVICE_PAUSED          =              0x00000007
+
+    # Service control manager access options
+    SC_MANAGER_CONNECT              = 0x0001
+    SC_MANAGER_ENUMERATE_SERVICE    = 0x0004
+    SC_MANAGER_ALL_ACCESS           = 0x000f003f
+
+    # Service access options
+    SERVICE_QUERY_CONFIG           = 0x0001
+    SERVICE_CHANGE_CONFIG          = 0x0002
+    SERVICE_QUERY_STATUS           = 0x0004
+    SERVICE_ENUMERATE_DEPENDENTS   = 0x0008
+    SERVICE_START                  = 0x0010
+    SERVICE_STOP                   = 0x0020
+    SERVICE_PAUSE_CONTINUE         = 0x0040
+    SERVICE_INTERROGATE            = 0x0080
+    SERVICE_USER_DEFINED_CONTROL   = 0x0100
+
+    # Windows error codes
+    ERROR_INSUFFICIENT_BUFFER   = 122
+    GENERIC_EXECUTE             = 0x20000000
+    GENERIC_WRITE               = 0x40000000
+    GENERIC_READ                = 0x80000000  
+
+    def declare_fnc(ct_dll, func_name, ret=None, args=[]):
+        fnc = getattr(ct_dll, func_name)
+        fnc.restype = ret
+        fnc.argtypes = args
+        return fnc
+
+    _advapi32 = ct.windll.advapi32
+
+    _OpenSCManager = declare_fnc(_advapi32, "OpenSCManagerA", ct.c_void_p,          \
+        [ct.c_char_p, ct.c_char_p, ct.c_ulong])
+
+    _OpenService = declare_fnc(_advapi32, "OpenServiceA", ct.c_void_p,              \
+        [ct.c_void_p, ct.c_char_p, ct.c_ulong])
+
+    _CloseServiceHandle = declare_fnc(_advapi32, "CloseServiceHandle", ct.c_bool,   \
+        [ct.c_void_p])
+
+    SC_STATUS_PROCESS_INFO = 0  # QueryServiceStatusEx level 
+    _QueryServiceStatusEx = declare_fnc(_advapi32, "QueryServiceStatusEx",  ct.c_bool,  \
+        [ct.c_void_p, ct.c_ulong, ct.POINTER(SERVICE_STATUS_PROCESS), ct.c_void_p, ct.c_void_p])
+
+    _EnumServicesStatus = declare_fnc(_advapi32, "EnumServicesStatusA", ct.c_bool,  \
+        [ct.c_void_p, ct.c_ulong, ct.c_ulong, ct.c_char_p, ct.c_ulong, ct.c_void_p, ct.c_void_p, ct.c_void_p])
+
+
     def __init__(self):
         pass
 
@@ -101,70 +118,45 @@ class WinServiceUtils:
     # advapi32.dll
     #
     def CloseServiceHandle(self, handle):
-        return _CloseServiceHandle(handle)
+        return self._CloseServiceHandle(handle)
 
     def OpenSCManager(self, machine_name = None, database_name = None, desired_access = 0):
         machine_name = bytes(machine_name, "mbcs") if machine_name else None
         database_name = bytes(database_name, "mbcs") if database_name else None
-        return _OpenSCManager(machine_name, database_name, desired_access)
+        return self._OpenSCManager(machine_name, database_name, desired_access)
 
     def OpenService(self, scm_h, name, desired_access = 0):
         name = bytes(name, "mbcs") if name  else None
-        return _OpenService(scm_h, name, desired_access)
+        return self._OpenService(scm_h, name, desired_access)
     
     def WindowsError(self, text):
         return RuntimeError(f"{text}. {ct.WinError().strerror} (ct.WinError().winerror)")
 
-    def get_service_status(self, s):
-        return {  
-            "service_type": s.service_type,
-            "current_state": s.current_state,
-            "controls_accepted": s.controls_accepted,
-            "win_exit_code": s.win_exit_code,
-            "service_exit_code": s.service_exit_code,
-            "check_point": s.check_point,
-            "wait_hint": s.wait_hint,
-            "process_id": s.process_id,
-            "service_flags": s.service_flags
-        }
-
-    def get_service_info(self, s):
-        return {  
-            "service_name": bytes.decode(s.service_name),
-            "display_name": bytes.decode(s.display_name),
-            "service_status": s.service_status,
-            "service_type": s.service_type,
-            "current_state": s.current_state,
-            "controls_accepted": s.controls_accepted,
-            "win_exit_code": s.win_exit_code,
-            "service_exit_code": s.service_exit_code,
-            "check_point": s.check_point,
-            "wait_hint": s.wait_hint,
-        }
-
     def QueryServiceStatusEx(self, name):
-        scm_h = self.OpenSCManager(None, None, SC_MANAGER_ALL_ACCESS) #SC_MANAGER_CONNECT+SC_MANAGER_ENUMERATE_SERVICE+GENERIC_READ)
+        scm_h = self.OpenSCManager(None, None, self.SC_MANAGER_CONNECT) #SC_MANAGER_CONNECT+SC_MANAGER_ENUMERATE_SERVICE+GENERIC_READ) SC_MANAGER_ALL_ACCESS
         if not scm_h:
             raise self.WindowsError("Failed to open service control")
 
-        service_h = self.OpenService(scm_h, name,SC_MANAGER_ALL_ACCESS)
+        service_h = self.OpenService(scm_h, name, self.SERVICE_QUERY_STATUS)
         if not service_h:
+            self.CloseServiceHandle(scm_h)
             raise self.WindowsError(f"Failed to open service {name}")
 
         status = SERVICE_STATUS_PROCESS()
         buf_size = ct.sizeof(status)
         bytes_needed = ct.c_ulong(0)
         ret = _QueryServiceStatusEx(service_h, SC_STATUS_PROCESS_INFO, ct.pointer(status), buf_size, ct.byref(bytes_needed))
-        if not ret:
-            raise self.WindowsError(f"Failed to query status of service {name}")
 
         self.CloseServiceHandle(service_h)
         self.CloseServiceHandle(scm_h)
 
+        if not ret:
+            raise self.WindowsError(f"Failed to query status of service {name}")
+
         return status
         
     def EnumServicesStatus(self, service_type, service_state):
-        scm_h = self.OpenSCManager(None, None, SC_MANAGER_CONNECT+SC_MANAGER_ENUMERATE_SERVICE)
+        scm_h = self.OpenSCManager(None, None, self.SC_MANAGER_CONNECT + self.SC_MANAGER_ENUMERATE_SERVICE)
 
         buf_size = 0
         bytes_needed = ct.c_ulong(0)
@@ -181,48 +173,4 @@ class WinServiceUtils:
 
         self.CloseServiceHandle(scm_h)
 
-        return { bytes.decode(svcs[i].service_name) : self.get_service_info(svcs[i]) for i in range(services_returned.value) }
-
-
-
-'''
-BOOL CloseServiceHandle(
-  [in] SC_HANDLE hSCObject
-);
-SC_HANDLE OpenSCManagerA(
-  [in, optional] LPCSTR lpMachineName,
-  [in, optional] LPCSTR lpDatabaseName,
-  [in]           DWORD  dwDesiredAccess
-);
-SC_HANDLE OpenServiceW(
-  [in] SC_HANDLE hSCManager,
-  [in] LPCSTR   lpServiceName,
-  [in] DWORD     dwDesiredAccess
-);
-BOOL EnumServicesStatusA(
-  [in]                SC_HANDLE              hSCManager,
-  [in]                DWORD                  dwServiceType,
-  [in]                DWORD                  dwServiceState,
-  [out, optional]     LPENUM_SERVICE_STATUSA lpServices,
-  [in]                DWORD                  cbBufSize,
-  [out]               LPDWORD                pcbBytesNeeded,
-  [out]               LPDWORD                lpServicesReturned,
-  [in, out, optional] LPDWORD                lpResumeHandle
-);
-typedef struct _ENUM_SERVICE_STATUSA {
-  LPSTR          lpServiceName;
-  LPSTR          lpDisplayName;
-  SERVICE_STATUS ServiceStatus;
-} ENUM_SERVICE_STATUSA, *LPENUM_SERVICE_STATUSA;
-
-BOOL QueryServiceStatusEx(
-  [in]            SC_HANDLE      hService,
-  [in]            SC_STATUS_TYPE InfoLevel,
-  [out, optional] LPBYTE         lpBuffer,
-  [in]            DWORD          cbBufSize,
-  [out]           LPDWORD        pcbBytesNeeded
-);
-
-
-'''
-
+        return { bytes.decode(svcs[i].service_name.lower(), "mbcs") : svcs[i] for i in range(services_returned.value) }
